efc0713e5ccdc07626973c5034e6c7527c3fce2b
Carl Ekman <carlekman@Carls-MacBook-Pro-2.local>
2013-02-18 18:27:24 +0100

    More small changes...

--- Panic/MainPanel.java
+++ Panic/MainPanel.java
@@ -1,5 +1,6 @@
 package Panic;
 
+import java.awt.GridBagConstraints;
 import java.awt.LayoutManager;
 
 import javax.swing.JPanel;
@@ -10,6 +11,15 @@ public class MainPanel extends JPanel {
 	TaskModiferView modiView;
 	public MainPanel(LayoutManager lm, LeftPanelView leftView, MidPanelView midView, RightPanelView rightView){
 		super(lm);
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 0;
+		c.gridy = 0;
+		add(leftView, c);
+		c.gridx = 1;
+		add(midView, c);
+		c.gridx = 2;
+		add(rightView, c);
 		catView = new CategoryItemView();
 		taskView = new TaskItemView();
 	}
