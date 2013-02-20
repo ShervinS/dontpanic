52d5faf4f329c68fc447b84dc2e057214510a227
Joseph Lundström <lundstromjoseph@gmail.com>
2013-02-20 15:24:22 +0100

    A menubar! Only exit works at this time.

--- Panic/Main.java
+++ Panic/Main.java
@@ -2,8 +2,13 @@ package Panic;
 
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Main {
 
@@ -18,8 +23,35 @@ public class Main {
 		
 		JFrame frame = new JFrame("!Panic");
 		frame.setContentPane(new MainPanel(pcLeft.getView(), pcMid.getView(), pcRight.getView()));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//frame.setContentPane(new MainPanel(new GridBagLayout(), pcLeft.getView(), pcMid.getView(), pcRight.getView()));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JMenuBar menuBar = new JMenuBar();
+		JMenu file = new JMenu("File");
+		JMenu edit = new JMenu("Edit");
+		JMenu help = new JMenu("Help");
+		
+		JMenu language = new JMenu("Change Language");
+		JMenuItem swedish = new JMenuItem("Swedish");
+		JMenuItem english = new JMenuItem("English");
+		
+		JMenuItem exit = new JMenuItem("Exit");
+		exit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		language.add(swedish);
+		language.add(english);
+		edit.add(language);
+		file.add(exit);
+		
+		menuBar.add(file);
+		menuBar.add(edit);
+		menuBar.add(help);
+		frame.setJMenuBar(menuBar);
+		
 		
 		pcLeft.updateGUI();
 		
