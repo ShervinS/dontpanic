03a6b279c210ee4d589321b7969b7f4495aab73a
Joseph Lundström <lundstromjoseph@gmail.com>
2013-02-21 12:36:09 +0100

    Saves the size of the window!

--- Panic/Main.java
+++ Panic/Main.java
@@ -4,6 +4,12 @@ import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -21,10 +27,34 @@ public class Main {
 		PanicController panicController = new PanicController(pcLeft, pcMid, pcRight);
 		pcLeft.setPanicController(panicController);
 		
-		JFrame frame = new JFrame("!Panic");
+		final JFrame frame = new JFrame("!Panic");
 		frame.setContentPane(new MainPanel(pcLeft.getView(), pcMid.getView(), pcRight.getView()));
 		//frame.setContentPane(new MainPanel(new GridBagLayout(), pcLeft.getView(), pcMid.getView(), pcRight.getView()));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				try {
+					Properties p = new Properties();
+					p.setProperty("WindowX", String.valueOf(frame.getWidth()));
+					p.setProperty("WindowY", String.valueOf(frame.getHeight()));
+					String saveLocation = System.getProperty("user.home") + "/.TODO-group9/config.properties";
+					
+					File saveFile = new File(saveLocation);
+					if (!saveFile.exists()) {
+						saveFile.getParentFile().mkdirs();
+						saveFile.createNewFile();
+					}
+					p.store(new FileOutputStream(saveFile), "Settings for TODO-application");
+				}
+				catch(Exception ex) {
+					System.err.println("Could not save.");
+					ex.printStackTrace();
+				}
+				finally {
+					System.exit(0);
+				}
+			}
+		});
 		JMenuBar menuBar = new JMenuBar();
 		JMenu file = new JMenu("File");
 		JMenu edit = new JMenu("Edit");
@@ -55,7 +85,23 @@ public class Main {
 		
 		pcLeft.updateGUI();
 		
-		frame.setSize(new Dimension(600,600));
-		frame.setVisible(true);		
+		int x = 600;
+		int y = 600;
+		try {
+			Properties p = new Properties();
+			String saveLocation = System.getProperty("user.home") + "/.TODO-group9/config.properties";
+			p.load(new FileInputStream(new File(saveLocation)));
+			x = Integer.parseInt(p.getProperty("WindowX"));
+			y = Integer.parseInt(p.getProperty("WindowY"));
+			frame.setSize(new Dimension(x, y));
+		}
+		catch (Exception e) {
+			System.err.println("Could not load configuration, using default values");
+		}
+		finally {
+			frame.setSize(new Dimension(x, y));
+			frame.setVisible(true);
+		}
+			
 	}
 }
